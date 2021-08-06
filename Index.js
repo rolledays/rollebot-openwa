4b2252f9cb626d7c4a56336198247c46a988b14c
Seventz <88520567+rolledays@users.noreply.github.com>
2021-08-06 09:49:51 +0700

    Index.js
    @Open.wa

---
+++ Index.js
@@ -0,0 +1,110 @@
+const { create, Client } = require('@open-wa/wa-automate')
+const welcome = require('./lib/welcome')
+const left = require('./lib/left')
+const cron = require('node-cron')
+const color = require('./lib/color')
+const fs = require('fs')
+const figlet = require('figlet')
+const lolcatjs = require('lolcatjs')
+const options = require('./options')
+//THANKS Tobz&Nurutomo//
+
+require('./seventz.js')
+nocache('./seventz.js', module => console.log(`'${module}' Updated!`))
+require('./data/command.js')
+nocache('./data/command.js', module => console.log(`'${module}' Updated!`))
+require('./lib/database/setting.json')
+nocache('./lib/database/setting.json', module => console.log(`'${module}' Updated!`))
+
+const adminNumber = JSON.parse(fs.readFileSync('./lib/database/admin.json'))
+const setting = JSON.parse(fs.readFileSync('./lib/database/setting.json'))
+const isWhite = (chatId) => adminNumber.includes(chatId) ? true : false
+
+let { 
+    limitacount,
+    limitmember, 
+    limitgroup,
+    mtc: mtcState,
+    isbanned,
+    restartState: isRestart
+    } = setting
+
+function restartbot(Ventz){
+    setting.restartState = false
+    isRestart = false
+    client.sendText(setting.restartId, 'Restart Succesfull!')
+    setting.restartId = 'undefined'
+    //fs.writeFileSync('./lib/setting.json', JSON.stringify(setting, null,2));
+}
+
+
+const start = async (Seventz = new Client()) => {
+        console.log('------------------------------------------------')
+        lolcatjs.fromString(color(figlet.textSync('SEVENTZ BOT', { horizontalLayout: 'full' })))
+        console.log('------------------------------------------------')
+        lolcatjs.fromString('[BY] Rolledays')
+        lolcatjs.fromString('[SERVER] Server Started!')
+        client.onAnyMessage((fn) => messageLog(fn.fromMe, fn.type))
+        // Force it to keep the current session
+        client.onStateChanged((state) => {
+            console.log('[Client State]', state)
+            if (state === 'CONFLICT' || state === 'UNLAUNCHED') client.forceRefocus()
+        })
+        // listening on message
+        client.onMessage((async (message) => {
+
+        client.getAmountOfLoadedMessages() // Pemotongan pesan dalam 3ribu teks
+            .then((msg) => {
+                if (msg >= 1000) {
+                    console.log('[CLIENT]', color(`Loaded Message Reach ${msg}, cuting message cache...`, 'yellow'))
+                    client.cutMsgCache()
+                }
+            })
+        // msgHndlr(client, message)
+        // Message Handler (Loaded from recent cache)
+        require('./seventz.js')(client, message)
+    }))
+
+        /*client.onAck((x => {
+            const { from, to, ack } = x
+            if (x !== 3) client.sendSeen(to)
+        }))*/
+
+        // Merespon panggilan
+        client.onIncomingCall(( async (call) => {
+            await client.sendText(call.peerJid, 'Ini Akun bot tidak bisa menerima panggilan!')
+            .then(() => client.contactBlock(call.peerJid))
+        }))
+    }
+
+/**
+ * Uncache if there is file change
+ * @param {string} module Module name or path
+ * @param {function} cb <optional> 
+ */
+function nocache(module, cb = () => { }) {
+    console.log('Module', `'${module}'`, 'is now being watched for changes')
+    fs.watchFile(require.resolve(module), async () => {
+        await uncache(require.resolve(module))
+        cb(module)
+    })
+}
+
+/**
+ * Uncache a module
+ * @param {string} module Module name or path
+ */
+function uncache(module = '.') {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(module)]
+            resolve()
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+create(options(true, start))
+    .then(client => start(client))
+    .catch((error) => console.log(error))
